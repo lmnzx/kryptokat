487c5a6abcb1bba786d672d4fe5a6203e264a20b
Sayan Mallick <snyxmk@gmail.com>
2022-06-12 11:08:46 +0530

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
-const Home = () => <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const Home = () => (
+  <div>
+    <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello NFT Marketplace
+    </h1>
+  </div>
+);
 
 export default Home;

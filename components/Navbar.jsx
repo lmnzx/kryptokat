d3d450d65beceb9275c85bb003411e630476c85b
Sayan Mallick <snyxmk@gmail.com>
2022-06-16 10:46:21 +0530

    added assets and theme switcher

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+import images from '../assets';
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  console.log({ theme });
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-dark border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div className="flexCenter md:hidden cursor-pointer" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+            <p className="dark:text-white text-nft-dark font-semibold text-lg ml-1">KryotoKat</p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex cursor-pointer" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input type="checkbox" className="checkbox" id="checkbox" onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')} />
+          <label htmlFor="checkbox" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;

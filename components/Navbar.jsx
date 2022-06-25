b21452b9555a1947508070b48daa253e71e6f553
Sayan Mallick <snyxmk@gmail.com>
2022-06-25 06:36:40 +0530

    NavBar Completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,17 +5,29 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import images from '../assets';
+import { Button } from '.';
 
 const MenuItems = ({ isMobile, active, setActive }) => {
-  const generateLink = () => {
-
+  const generateLink = (i) => {
+    switch (i) {
+      case 0: return '/';
+      case 1: return '/created-nft';
+      case 2: return '/my-nfts';
+      default: return '/';
+    }
   };
 
   return (
     <ul className={`list-none flexCenter flex-row ${isMobile && 'flex-col h-full'}`}>
       {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, i) => (
-        <li key={i} onClick={() => {}} className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${active === item ? 'dark:text-white text-nft-black-1' : 'dark:text-nft-gray-3 text-nft-gray-2'}`}>
-          <Link href="">
+        <li
+          key={i}
+          onClick={() => {
+            setActive(item);
+          }}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${active === item ? 'dark:text-white text-nft-black-1' : 'dark:text-nft-gray-3 text-nft-gray-2'}`}
+        >
+          <Link href={generateLink(i)}>
             {item}
           </Link>
         </li>
@@ -24,9 +36,33 @@ const MenuItems = ({ isMobile, active, setActive }) => {
   );
 };
 
+const ButtonGroup = ({ setActive, router }) => {
+  const hasConnected = true;
+
+  return hasConnected ? (
+    <Button
+      btnName="Create"
+      classStyles="rounded-xl mx-2"
+      handleClick={() => {
+        setActive('');
+
+        router.push('/create-nft');
+      }}
+    />
+  ) : (
+    <Button
+      btnName="Connect"
+      classStyles="rounded-xl mx-2"
+      handleClick={() => {}}
+    />
+  );
+};
+
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const [active, setActive] = useState('Explore NFTs');
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-dark border-nft-gray-1">
@@ -52,11 +88,44 @@ const Navbar = () => {
             <div className="w-3 h-3 absolute bg-white rounded-full ball" />
           </label>
         </div>
-      </div>
-      <div className="md:hidden flex">
-        <ul className="list-none flexCenter flex-row">
+        <div className="md:hidden flex">
           <MenuItems active={active} setActive={setActive} />
-        </ul>
+          <ButtonGroup setActive={setActive} router={router} />
+        </div>
+      </div>
+      {/* Mobile nav */}
+      <div className="hidden md:flex ml-2">
+        {isOpen ? (
+          <Image
+            src={images.cross}
+            objectFit="contain"
+            width={20}
+            height={20}
+            alt="close"
+            onClick={() => setIsOpen(false)}
+            className={theme === 'light' && 'filter invert'}
+          />
+        ) : (
+          <Image
+            src={images.menu}
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="menu"
+            onClick={() => setIsOpen(true)}
+            className={theme === 'light' && 'filter invert'}
+          />
+        )}
+        {isOpen && (
+          <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+            <div className="flex-1 p-4">
+              <MenuItems active={active} setActive={setActive} isMobile />
+            </div>
+            <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+              <ButtonGroup setActive={setActive} router={router} />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
